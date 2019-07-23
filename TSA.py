c687bf72ae550fc5d001ed99c317cf4dbcbd0467
Deepak Hirawat <bit.deepak99@gmail.com>
2019-07-24 00:16:05 +0530

    minor changes

--- TSA.py
+++ TSA.py
@@ -3,7 +3,6 @@ import pandas as pd
 from api import TestDataSet
 
 TrainingData = pd.read_csv('tweet_training_set.csv' , encoding='unicode_escape')
-TrainingData.columns = ['id' , 'text' , 'label' , 'topic']
 test = TestDataSet(query='narendramodi')
 TestData = test.buildTestSet()
 # for item in TestData[0:4]:
